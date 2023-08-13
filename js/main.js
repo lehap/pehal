c0f3a1e3a59b91dcd97b8804c1974a5b92f989af
lehap <142183294+lehap@users.noreply.github.com>
2023-08-13 23:22:45 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const date = new Date().toLocaleDateString()
+const counter = document.querySelector('.counter')
+const title = document.querySelector('.title')
+
+
+const firstRoundDate = new Date("Oct 6, 2023 15:30:0").getTime()
+
+const rounds = [firstRoundDate]
+
+function counterFunc(){
+  
+  let dateNow = new Date().getTime()
+  let distance
+
+  
+  for(let i = 0; i < rounds.length; i++){
+    if(rounds[i] - dateNow < 0){
+      continue
+    }
+    distance = rounds[i] - dateNow
+    break
+  }
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+  
+
+  counter.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+
+  if (distance < 0) {
+    clearInterval(counterInterval);
+  }
+}
+counterFunc()
+
+const counterInterval = setInterval(counterFunc, 1000);
